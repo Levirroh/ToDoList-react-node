60323f5ef3f72be0a93317790b3678db4d6ef0fd
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-09-28 20:50:58 -0300

    addded repository

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    tailwindcss(),
+  ],
+}
